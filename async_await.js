e9b1b91611152aadb188ec84299d60a8644d0fc6
Atharva Barve <atharvabarve02@gmail.com>
2024-05-31 09:42:13 +0530

    updated

--- async_await.js
+++ async_await.js
@@ -1,5 +1,7 @@
 //callback function
 
+const async = require("hbs/lib/async")
+
 // function x(y) {
 //     y()
 //     console.log("x");
@@ -12,18 +14,31 @@
 
 //without async/await
 
-const p = new Promise((resolve, reject) => {
-    resolve("Promise Resolved")
-})
+// const p = new Promise((resolve, reject) => {
+//     resolve("Promise Resolved")
+// })
 
-getData = () => {
-    console.log("Before promise resolved");
-    p.then((res) => {
-        console.log(res);
-    })
-}
+// getData = () => {
+//     console.log("Before promise resolved");
+//     p.then((res) => {
+//         console.log(res);
+//     })
+// }
 
-getData()
+// getData()
 
 //with async/await
 
+// const p = new Promise((resolve, reject) => {
+//     setTimeout(()=>{
+//         resolve("Promise resolved")
+//     },5000)
+// })
+
+// async function handlePromise() {
+//     console.log("val");
+//     const val = await p
+//     console.log(val);
+// }
+
+// handlePromise()
